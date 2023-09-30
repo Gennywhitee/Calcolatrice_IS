7b25281b4012e51ac89b08983729547824af7717
Gennywhitee <genny.ambrosio03@gmail.com>
2023-09-30 13:19:03 +0200

    Upload Calcolatrice

---
+++ untitled/src/main/java/org/example/Calcolatrice.java
@@ -0,0 +1,17 @@
+package org.example;
+
+public class Calcolatrice {
+    public Calcolatrice(){
+        a = 0;
+        b = 0;
+    }
+
+    public double sum(double a, double b){
+        return a + b;
+    }
+
+    public double multiplication(double a, double b){
+        return a * b;
+    }
+    private double a,b;
+}
